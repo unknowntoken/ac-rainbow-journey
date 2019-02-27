4c8d3e93ce29429cc164c7fd51598aa7e345513f
Rui Teixeira <rui.c.teixeira.95@gmail.com>
2019-02-27 00:04:35 -0100

    Cols and Rows collision

--- src/org/academiadecodigo/tropadelete/cheiodesono/Collision.java
+++ src/org/academiadecodigo/tropadelete/cheiodesono/Collision.java
@@ -5,16 +5,54 @@ public abstract class Collision {
     private boolean isCrash;
     private Player player;
     private Obstacle obstacle;
+    private int rowPlayer;
+    private int colPlayer;
+    private int rowObstacle;
+    private int colObstacle;
 
-    public Collision(){
+
+    public Collision() {
         isCrash = false;
 
     }
 
-    public void collision(){
+    public void collision(Position player, Position obstacle) {
+        if (!isCrash) {
+            getRowPlayer(player);
+            getColPlayer(player);
+            getRowObstacle(obstacle);
+            getColObstacle(obstacle);
+            if (compareCol() && compareRow()) {
+                isCrash = true;
+            }
+        }
+
 
     }
 
+    private void getRowPlayer(Position player) {
+        this.rowPlayer = player.getX();
+    }
+
+    private void getColPlayer(Position player) {
+        this.colPlayer = player.getY();
+    }
+
+    private void getRowObstacle(Position obstacle) {
+        this.rowObstacle = obstacle.getX();
+    }
+
+    private void getColObstacle(Position obstacle) {
+        this.colObstacle = obstacle.getY();
+    }
+
+    private boolean compareRow() {
+        return rowPlayer == rowObstacle;
+    }
+
+    private boolean compareCol() {
+        return colPlayer == colObstacle;
+    }
 
     public boolean isCrash() {
         return isCrash;
