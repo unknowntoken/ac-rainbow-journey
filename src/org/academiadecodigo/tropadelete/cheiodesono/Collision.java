c4430053ff42f8418deb2afd40f217cdc72f9412
Kevin Dias <kevinsilveiradias@gmail.com>
2019-02-27 23:59:02 -0100

    Refactored collision class

--- src/org/academiadecodigo/tropadelete/cheiodesono/Collision.java
+++ src/org/academiadecodigo/tropadelete/cheiodesono/Collision.java
@@ -2,82 +2,18 @@ package org.academiadecodigo.tropadelete.cheiodesono;
 
 public abstract class Collision {
 
-    private boolean isCrash;
-    private Player player;
-    private Obstacle obstacle;
-    private int rowPlayer;
-    private int colPlayer;
-    private int widthPlayer;
-    private int heightPlayer;
-    private int rowObstacle;
-    private int colObstacle;
-    private int widthObstacle;
-    private int heightObstacle;
+    public static boolean collide(int xPosA, int yPosA, int widthA, int heightA,int xPosB, int yPosB, int widthB, int heightB){
+        return
+                //Checking for intersection of rectangle A with rectangleB
+                //Origin is top left corner of each rectangle
+                //If condition is true the rectangle A AND rectangle B intersect and thus collide
 
-
-    public Collision() {
-        isCrash = false;
-
+                xPosA + widthA > xPosB
+                        &&
+                yPosA +  heightA > yPosB
+                                        &&
+                xPosB + widthB > xPosA
+                         &&
+                yPosB + heightB > yPosA;
     }
-
-    public boolean isCollision(Position player, Position obstacle) {
-        if (!isCrash) {
-            getRowPlayer(player);
-            getColPlayer(player);
-            getRowObstacle(obstacle);
-            getColObstacle(obstacle);
-            if (compareCol() && compareRow()) {
-                isCrash = true;
-            }
-        }
-
-        return isCrash;
-    }
-
-    //*********************
-    //*******PLAYER********
-    //*********************
-    private void getRowPlayer(Position player) {
-        this.rowPlayer = player.getX();
-    }
-
-    private void getColPlayer(Position player) {
-        this.colPlayer = player.getY();
-    }
-
-    private void getWidthPlayer(Player player) {
-        //this.widthPlayer = player.
-    }
-
-    private void getHeightPlayer(Player player) {
-        //this.heightPlayer = player.
-    }
-    //*********************
-    //******OBSTACLE*******
-    //*********************
-    private void getRowObstacle(Position obstacle) {
-        this.rowObstacle = obstacle.getX();
-    }
-
-    private void getColObstacle(Position obstacle) {
-        this.colObstacle = obstacle.getY();
-    }
-
-    private void getWidthObstacle(Obstacle obstacle) {
-        //this.widthObstacle =obstacle.
-    }
-
-    private void getHeightObstacle(Obstacle obstacle){
-        //this.heightObstacle = obstacle.
-    }
-
-
-    private boolean compareRow() {
-        return rowPlayer == rowObstacle;
-    }
-
-    private boolean compareCol() {
-        return colPlayer == colObstacle;
-    }
-
 }
