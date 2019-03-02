049b5e9104cf0ff7ebd811c25df28eae4a4e2d8e
Kevin Dias <kevinsilveiradias@gmail.com>
2019-03-02 21:48:29 -0100

    finished sprite animation

--- src/org/academiadecodigo/tropadelete/cheiodesono/gameobjects/Player.java
+++ src/org/academiadecodigo/tropadelete/cheiodesono/gameobjects/Player.java
@@ -10,6 +10,7 @@ import org.academiadecodigo.tropadelete.cheiodesono.gameobjects.GameObject;
 
 public class Player implements GameObject {
     private static final int JUMP_HEIGHT = 200;
+    private static final int MAX_HEALTH = 10;
 
     private int health;
     private boolean jumping;
@@ -32,7 +33,7 @@ public class Player implements GameObject {
         jumpCounter = 0;
         down = true;
         health = 10;
-        sprite = new Sprite(50);
+        sprite = new Sprite(25);
         jumpSound = new Sound("/resources/sounds/jump.wav");
 
         initHealthBar();
@@ -179,6 +180,9 @@ public class Player implements GameObject {
 
     public void addHealth(int health) {
         this.health += health;
+        if(this.health >10){
+            this.health= 10;
+        }
         updateHealthBar();
         sprite.update();
     }
